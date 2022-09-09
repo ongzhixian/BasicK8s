7726272827e8ab9452b395993b8dbab5a45039ec
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-09-09 13:57:21 +0800

    update

--- e2e/devenv/setup.ps1
+++ e2e/devenv/setup.ps1
@@ -21,6 +21,22 @@ function Install-NewK8s {
     
 }
 
+function Sync-Secrets {
+
+    kubectl create secret generic mysql-secret --from-literal=PASSWORD=$((Get-Content "$env:USERPROFILE/.pythonanywhere.json" -Raw | ConvertFrom-Json).MYSQL.dev_forum.PASSWORD) -o yaml --dry-run=client > secret-mysql.yaml
+    kubectl apply -f .\secret-mysql.yaml
+
+    kubectl create secret generic cloud-amqp-secret --from-file=$env:USERPROFILE/.cloudampq.json -o yaml --dry-run=client > secret-cloud-amqp.yaml
+    kubectl apply -f .\secret-cloud-amqp.yaml
+
+    kubectl create secret generic mysql-settings-secret --from-file=$env:USERPROFILE/.pythonanywhere.json -o yaml --dry-run=client > secret-mysql-settings.yaml
+    kubectl apply -f .\secret-mysql-settings.yaml
+    
+    kubectl create secret generic database-config-secret --from-file=$env:USERPROFILE/.database-config.json -o yaml --dry-run=client > secret-database-config.yaml
+    kubectl apply -f .\secret-database-config.yaml
+
+}
+
 switch ($task) {
     'do-nothing' {
         exit 0
@@ -32,15 +48,15 @@ switch ($task) {
         Write-Host "Todo"
         exit 0
     }
+    'sync-secrets' {
+        Sync-Secrets
+    }
     Default {
         exit 1
     }
 }
 
-function Setup-Secrets {
-    param ()
-    Write-Host 'Setup secrets'
-}
+
 
 # Overview of things to setup
 # Namespace
@@ -76,19 +92,7 @@ function Examples {
     # kubectl create secret generic mysql-secret --from-literal=PASSWORD=pass1234 -o yaml --dry-run=client > secret-mysql.yaml
     # kubectl create secret generic cloud-amqp-secret --from-literal=CLOUD_AMQP_URL=$((Get-Content "$env:USERPROFILE/.cloudampq.json" -Raw | ConvertFrom-Json).cloud_amqp.armadillo.url) -o yaml --dry-run=client > secret-cloudamqp.yaml
 
-    kubectl create secret generic mysql-secret --from-literal=PASSWORD=$((Get-Content "$env:USERPROFILE/.pythonanywhere.json" -Raw | ConvertFrom-Json).MYSQL.dev_forum.PASSWORD) -o yaml --dry-run=client > secret-mysql.yaml
-    kubectl apply -f .\secret-mysql.yaml
-
-    kubectl create secret generic cloud-amqp-secret --from-file=$env:USERPROFILE/.cloudampq.json -o yaml --dry-run=client > secret-cloud-amqp.yaml
-    kubectl apply -f .\secret-cloud-amqp.yaml
-
-    kubectl create secret generic mysql-settings-secret --from-file=$env:USERPROFILE/.pythonanywhere.json -o yaml --dry-run=client > secret-mysql-settings.yaml
-    kubectl apply -f .\secret-mysql-settings.yaml
-    
-    kubectl create secret generic database-config-secret --from-file=$env:USERPROFILE/.database-config.json -o yaml --dry-run=client > secret-database-config.yaml
-    kubectl apply -f .\secret-database-config.yaml
-
-    Setup-Secrets
+    Sync-Secrets
     
 
     ########################################
