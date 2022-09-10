e73bc47fb3caa73c2a5721318a77215d237b7819
zhixian <zhixian@hotmail.com>
2022-09-10 22:39:30 +0800

    update

---
+++ database/sql/04-market_identifier.sql
@@ -0,0 +1,32 @@
+﻿/*
+Description:
+    Script to create `market_identifier` table in `financial` database.
+    
+History:
+
+Date            Description
+2022-09-07      Create `market_identifier` table
+
+*/
+
+USE `financial`;
+
+
+CREATE TABLE IF NOT EXISTS `market_identifier` (
+	`id` 			        VARCHAR(8) NOT NULL,
+	`alias`					VARCHAR(8) NULL,
+	`ric_suffix`			VARCHAR(8) NULL,
+	`is_iso`				TINYINT(1) NOT NULL,
+	`cre_dt` 				DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+	`upd_dt` 				DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8mb4_unicode_ci'
+;
+
+-- Seed data
+INSERT INTO market_identifier(id, alias, ric_suffix, is_iso) VALUES
+    ('XSES', 'SGX', 	'SI', 1),
+	('XASX', 'ASX', 	'AX', 1),
+	('XSFE', 'SFE', 	NULL, 1),
+    ('AORA', 'OANDA', 	NULL, 0);
---
+++ database/sql/05-instrument.sql
@@ -0,0 +1,33 @@
+﻿/*
+Description:
+    Script to create `instrument` table in `financial` database.
+    
+History:
+
+Date            Description
+2022-09-07      Create `instrument` table
+
+INSERT INTO `instrument` (market_identifier_id, code, name, counter, isin) VALUES (?, ?, ?, ?, ?)
+sgx_isin_layout = r"(?P<name>.{50})(?P<status>.{10})(?P<isin>.{20})(?P<code>.{10})(?P<counter>.+)"
+
+*/
+
+USE `financial`;
+
+
+CREATE TABLE IF NOT EXISTS `instrument` (
+	`id`					INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`market_identifier_id` 	VARCHAR(8) NOT NULL,
+	`code` 			        VARCHAR(10) NOT NULL,
+	`name` 			        VARCHAR(50) NOT NULL,
+	`counter` 			    VARCHAR(30) NULL,
+	`isin` 			        VARCHAR(20) NULL,
+	`exclusion_id`			INT UNSIGNED NULL,
+	`cre_dt` 				DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+	`upd_dt` 				DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX `name_u_idx` (`market_identifier_id`, `code`)
+)
+COLLATE='utf8mb4_unicode_ci'
+;
+
